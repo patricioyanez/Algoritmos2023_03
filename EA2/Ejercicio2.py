edd338293f71e3888c84d258227f43a93d021e7d
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-04-10 11:58:38 -0400

    update

---
+++ EA2/Ejercicio2.py
@@ -0,0 +1,10 @@
+print("Ingrese los siguientes datos:")
+nombre = input("Nombre:")
+rut = input("Rut:")
+correo = input("Correo:")
+telefono = input("Télefono:")
+
+print("Nombre:" , nombre)
+print(f"Rut: {nombre}" )
+print("Correo: ",correo )
+print("Télefono:",telefono )
