19edb586168fcfc0f499081ed6130bdbdf10d1b5
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-04-11 09:45:15 -0400

    Update Ejercicio3RangoEtario.py

--- EA2/Ejercicio3RangoEtario.py
+++ EA2/Ejercicio3RangoEtario.py
@@ -1,2 +1,14 @@
 # Solicitar edad y clasifiquelo según rango etario
 # niño, adolescente, adulto, adulto mayor
+print("Clasificación por rango etario")
+edad = input("Ingrese su edad:")
+edad = int(edad)
+
+if edad <= 12:
+    print("Ud. es un niño")
+elif edad < 19:
+    print("Ud. es Adolescente")
+elif edad < 65:
+    print("Ud. es Adulto")
+else:
+    print("Ud. está en la 3ra edad")
