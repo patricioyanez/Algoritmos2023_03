8a26c9b9986cae997bd6593a3b4536bd8c8aa8f9
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-04-24 10:37:55 -0400

    Update EjercicioFor1.py

--- EA2/EjercicioFor1.py
+++ EA2/EjercicioFor1.py
@@ -2,3 +2,21 @@
 # Cuántos números son mayores a cero
 # Cuántos números son menores a cero
 # Cuántos números son iguales a cero
+mayoresACero = 0
+menoresACero = 0
+igualesACero = 0
+rango = range(5)
+for indice in rango:
+    numero = int(input("Ingrese número: "))
+
+    if numero > 0:
+        mayoresACero += 1 # contador
+    elif numero < 0:
+        menoresACero += 1
+    else:
+        igualesACero += 1
+    
+    
+print("Cantidad de mayores a cero: ", mayoresACero)
+print("Cantidad de menores a cero: ", menoresACero)
+print("Cantidad de iguales a cero: ", igualesACero)
