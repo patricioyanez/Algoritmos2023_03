63a5ba224a2559fa829f3dff5e9338fce73f2e11
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-05-02 09:53:35 -0400

    update

--- EA2/cicloWhileMenu.py
+++ EA2/cicloWhileMenu.py
@@ -1,19 +1,37 @@
 opcion = ""
 
-while opcion != "3":
-    print("******* Calculadora *******")
+while opcion != "5":
+    print("******* Calculadora *******") ## multiplicar y dividir
     print("1.- Sumar")
     print("2.- Restar")
-    print("3.- Salir")
+    print("3.- Multiplicar")
+    print("4.- Dividir")
+    print("5.- Salir")
 
     opcion = input("Ingrese opción:")
 
-    # si NO ESTA en los caracteres: 1,2 o 3 (como string) no es válido
-    if opcion not in ("1", "2", "3"):
+    # si NO ESTA en los caracteres: 1,2, 3, 4 o 5 (como string) no es válido
+    if opcion not in ("1", "2", "3", "4", "5"):
         print("Opción no es válida")
     elif opcion == "1":
         n1 = int(input("Ingrese 1er número:"))
         n2 = int(input("Ingrese 2do número:"))
         print("La suma de los números es:", (n1+n2))
+    elif opcion == "2":
+        n1 = int(input("Ingrese 1er número:"))
+        n2 = int(input("Ingrese 2do número:"))
+        print("La resta de los números es:", (n1-n2))
+    elif opcion == "3":
+        n1 = int(input("Ingrese 1er número:"))
+        n2 = int(input("Ingrese 2do número:"))
+        print("La mulitiplicación de los números es:", (n1*n2))
+    elif opcion == "4":
+        n1 = int(input("Ingrese 1er número:"))
+        n2 = int(input("Ingrese 2do número:"))
+
+        if n2 == 0:
+            print("No se puede dividir por cero, vuelva a intentar")
+        else:
+            print("La división de los números es:", (n1/n2))
     
     
