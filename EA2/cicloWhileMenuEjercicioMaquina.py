b79d7a619cc1e32ba014a3c53a6091657ea6d96d
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-05-08 08:56:08 -0400

    ejercicio

---
+++ EA2/cicloWhileMenuEjercicioMaquina.py
@@ -0,0 +1,10 @@
+#I.- Caso: Máquina de Bebidas
+
+# En su lugar de trabajo existe una máquina donde puede adquirir bebidas. Existen tres 
+# opciones de bebida: Coca-Cola, Fanta y Sprite. El valor de cualquiera de ellas es: $400.
+# Desarrolle un programa que permita vender una bebida y entregar vuelto si así se determina. El
+# programa debe solicitar por pantalla el tipo de bebida que va a comprar: 1) Coca-Cola, 2) Fanta o 3) Sprite (validar opción). Además, se debe pedir el dinero a pagar por la bebida.
+# -	Si el dinero es igual a $400 no tiene vuelto.
+# -	Si el dinero es mayor a $400 debe calcular y entregar vuelto.
+# -	Si el dinero es menor a $400 debe mandar un mensaje de alerta y no entregar la bebida.
+# Finalizado el proceso, esperar una nueva orden.
