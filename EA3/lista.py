d7d2147eff1f9fac1bd30caa7a2566fd997b80ec
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-05-22 10:42:17 -0400

    lista

---
+++ EA3/lista.py
@@ -0,0 +1,15 @@
+lista = [1,2,3,4] # indice inicia del cero
+
+print(lista)
+
+print("valor del indice 1 (parte del cero): ", lista[1])
+print("valor del indice 3 (parte del cero): ", lista[3])
+
+# indice  : 0       1       2    3    4 
+lista2 = ["hola", "CHAO", True, 1.5, 100]
+
+print("Valor del índice 1: ", lista2[1])
+print("Valor del índice 4: ", lista2[4])
+print("Valor del índice 4: ", lista2[-1])
+print("Valor del índice 3: ", lista2[-2])
+
